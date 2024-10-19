1c73a8d00318f763530c39fd5ae5f0598803adc3
P9DRIN <pedrohenriquelimasilva2837@hotmail.com>
2024-10-19 17:24:32 -0300

    fix: fixed envs

--- dist/env.js
+++ dist/env.js
@@ -3,7 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.envSchema = void 0;
 const zod_1 = require("zod");
 exports.envSchema = zod_1.z.object({
-    DATABASE_URL: zod_1.z.string().url('mongodb+srv://admin:123doafacil@cluster0.3mj5p.mongodb.net'),
+    DATABASE_URL: zod_1.z.string().url(),
     PORT: zod_1.z.coerce.number().optional().default(8000),
     JWT_PRIVATE_KEY: zod_1.z.string(),
     JWT_PUBLIC_KEY: zod_1.z.string(),
