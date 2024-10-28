8809fa460ed537fac581e6128cd834217b4f4816
Lima Silva <pedrohenriquelimasilva2837@hotmail.com>
2024-10-28 16:05:16 -0300

    allow cors for all connections

--- src/main.ts
+++ src/main.ts
@@ -18,9 +18,7 @@ async function bootstrap() {
   app.use(cookieParser());
 
   app.enableCors({
-    origin: '*',
-    // origin: 'https://doa-facil.vercel.app',
-    // origin: 'http://localhost:5173',
+    origin: ['http://localhost:5173', 'https://doa-facil.vercel.app'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
