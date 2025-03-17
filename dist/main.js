c2f1ad3af742f9d2d760ebcbf28005745fa34174
Lima <pedrohenriquelimasilva2837@hotmail.com>
2025-03-16 21:08:23 -0300

    fix cors

--- dist/main.js
+++ dist/main.js
@@ -15,19 +15,20 @@ async function bootstrap() {
     dotenv.config();
     app.use(cookieParser());
     app.enableCors({
-        origin: ['http://localhost:5173', 'https://doa-facil.vercel.app'],
+        origin: (origin, callback) => {
+            const allowedOrigins = [
+                'http://localhost:5173',
+                'https://doa-facil.vercel.app',
+            ];
+            if (allowedOrigins.includes(origin) || !origin) {
+                callback(null, true);
+            }
+            else {
+                callback(new Error('Not allowed'));
+            }
+        },
         methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-        allowedHeaders: ['Content-Type', 'Authorization'],
         credentials: true,
-    });
-    app.use((req, res, next) => {
-        if (req.method === 'OPTIONS') {
-            res.header('Access-Control-Allow-Origin', req.headers.origin || '*');
-            res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS');
-            res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-            return res.sendStatus(204);
-        }
-        next();
     });
     app.useGlobalPipes(new common_1.ValidationPipe());
     console.log('PORT:', port);
--- src/main.ts
+++ src/main.ts
@@ -18,35 +18,20 @@ async function bootstrap() {
   app.use(cookieParser());
 
   app.enableCors({
-    // origin: (origin, callback) => {
-    //   const allowedOrigins = [
-    //     'http://localhost:5173',
-    //     'https://doa-facil.vercel.app',
-    //   ];
-    //   if (allowedOrigins.includes(origin) || !origin) {
-    //     callback(null, true);
-    //   } else {
-    //     callback(new Error('Not allowed'));
-    //   }
-    // },
+    origin: (origin, callback) => {
+      const allowedOrigins = [
+        'http://localhost:5173',
+        'https://doa-facil.vercel.app',
+      ];
+      if (allowedOrigins.includes(origin) || !origin) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed'));
+      }
+    },
     // origin: 'http://localhost:5173',
-    origin: ['http://localhost:5173', 'https://doa-facil.vercel.app'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
-  });
-
-  app.use((req, res, next) => {
-    if (req.method === 'OPTIONS') {
-      res.header('Access-Control-Allow-Origin', req.headers.origin || '*');
-      res.header(
-        'Access-Control-Allow-Methods',
-        'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-      );
-      res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-      return res.sendStatus(204);
-    }
-    next();
   });
 
   app.useGlobalPipes(new ValidationPipe());
