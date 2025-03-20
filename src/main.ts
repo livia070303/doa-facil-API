3db2a102a0dc40ba9fc2f31eeadac55821e2f0cc
Lima <pedrohenriquelimasilva2837@hotmail.com>
2025-03-20 19:38:03 -0300

    fix: cors

--- src/main.ts
+++ src/main.ts
@@ -25,23 +25,6 @@ async function bootstrap() {
     credentials: true,
   });
 
-  app.use((req, res, next) => {
-    res.setHeader(
-      'Access-Control-Allow-Origin',
-      'https://doa-facil.vercel.app',
-    );
-    res.setHeader(
-      'Access-Control-Allow-Methods',
-      'GET, POST, OPTIONS, PUT, PATCH, DELETE',
-    );
-    res.setHeader(
-      'Access-Control-Allow-Headers',
-      'Content-Type, Authorization',
-    );
-    res.setHeader('Access-Control-Allow-Credentials', 'true');
-    next();
-  });
-
   app.useGlobalPipes(new ValidationPipe());
 
   app.useWebSocketAdapter(new IoAdapter(app));
